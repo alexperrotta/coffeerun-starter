8d6d233f634a88e290734569aa4660ad4225b72d
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-04-12 21:17:07 -0400

    rendered list

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,39 @@
 $(function(){
 
+	function renderCoffeeOrder(order) {
+		var finalHTML = '<div class="order">';
+
+		finalHTML += '<span>'+ order.coffeeOrder +'</span>';
+		finalHTML += '<span>'+ order.email +'</span>';
+		finalHTML += '<span>'+ order.size +'</span>';
+		finalHTML += '<span>'+ order.flavorShot +'</span>';
+		finalHTML += '<span>'+ order.strength +'</span>';
+
+		finalHTML += '</div';
+
+		return finalHTML;
+	}
+
 	//*** Listen for when people submit the form ***//
 
-	$('form').submit(function(){
+	$('form').submit(function(e){
 		// code inside here will execute when the form is submitted
-		var coffeeOrder = $('#coffeeOrder').val();
-		var email = $('#emailInput').val();
-		var size = $('#input:checked').val();
-		var flavorShot = $('#flavorShot').val();
-		var strength = $('#strengthLevel').val();
+		e.preventDefault();
 
-		console.log(coffeeOrder);
-		console.log(email);
-		console.log(size);
-		console.log(flavorShot);
-		console.log(strength);
+		var currentOrder = {
+			coffeeOrder: $('#coffeeOrder').val(),
+			email: $('#emailInput').val(),
+			size: $('input:checked').val(),
+			flavorShot: $('#flavorShot').val(),
+			strength: $('#strengthLevel').val(),
+		};
+
+		// take the above above, render it to the screen
+		var renderedHTML = renderCoffeeOrder(currentOrder);
+
+		// get the renderedHTML to show up in the DOM
+		$('#orderList').append(renderedHTML);
+
 
 	});
 
